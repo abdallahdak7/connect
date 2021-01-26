a585ca6a7ba26779d86a563b556c6d23af5629ed
Abdallah Daknache <abdallah_daknache@hotmail.com>
2021-01-26 14:30:45 +0200

    configured database and server

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// connect database
+connectDB();
 
 app.get('/', (req, res) => res.send('API running !'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`The server has been started on port ${PORT} !`),
+);
