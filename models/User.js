f711dfc017aa59c527f6af684d8284b7cdd1fa82
Abdallah Daknache <abdallah_daknache@hotmail.com>
2021-01-28 21:50:03 +0200

    added jwt, get profiles, create,update profiles

--- models/User.js
+++ models/User.js
@@ -10,7 +10,7 @@ const UserSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
-  pasword: {
+  password: {
     type: String,
     required: true,
   },
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const User = require('../../models/User');
 
 // @route   POST api/users
@@ -27,8 +31,46 @@ router.post(
     try {
       let user = await User.findOne({ email });
 
-      user &&
-        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+      if (user)
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+      });
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      // creates a user in the db, and return it.
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 3600,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        },
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
